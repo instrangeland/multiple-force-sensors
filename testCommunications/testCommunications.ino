58ef4c4760a6383bd48f82c08b4ffc38c4dc4ba0
instrangeland <35543776+instrangeland@users.noreply.github.com>
2018-04-21 16:29:25 -0400

    Setup Things for Arduino IDE

--- testCommunications/testCommunications.ino
+++ testCommunications/testCommunications.ino
@@ -1,21 +1,21 @@
 int buffer[10];
 const byte interruptPin = 13;
-bool stopwatchRunning=0; 
+bool stopwatchRunning = 0;
 int nextBuffer = 1;
 int nextReadBuffer = 0;
 const byte ledPin = 13;
 int tempBuffer[2];
-SoftwareSerial inSerial = SoftwareSerial(, txPin);
+
 void setup()
 {
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), sendInfo, CHANGE);
-  byte arduino_Number=1; // change for each arduino
+  byte arduino_Number = 1; // change for each arduino
   Wire.begin(); // join i2c bus (address optional for master)
   //TWBR = 12; //Increase i2c speed if you have Arduino MEGA2560, not suitable for Arduino UNO
   Serial.begin(57600);  // start serial for output
   Serial.flush();
-  
+
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -23,38 +23,39 @@ void setup()
 }
 int readBuffer()
 {
-  if (nextReadBuffer<nextBuffer)
+  if (nextReadBuffer != nextBuffer)
   {
-    return [buffer[nextReadBuffer],0]
+    tempBuffer[1]=buffer[nextReadBuffer];
+    tempBuffer[2]=0;
     nextReadBuffer++;
   }
   else
   {
-    return[0,1]
+    tempBuffer[1]=0;
+    tempBuffer[2]=1;
   }
 }
 void addToBuffer( int number )
 {
   buffer[nextBuffer] = number;
   nextBuffer++;
-  if (nextBuffer>10)
+  if (nextBuffer > 10)
   {
-    nextBuffer=1;
+    nextBuffer = 1;
   }
 }
 void sendInfo()
 {
-
-    addToBuffer(Arduino_Number); 
+  addToBuffer(Arduino_Number);
 }
 void loop()
 {
-  if (Serial.available()>0)
+  if (Serial.available() > 0)
   {
     addToBuffer(Serial.read());
   }
-  tempBuffer=readBuffer();
-  if (tempBuffer[2]=0)
+  sendInfo();
+  if (tempBuffer[2] = 0)
   {
     serial.write(tempBuffer[1]);
   }
