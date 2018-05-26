e581ecb9a2d95441d6892fe4505923f7a9f35e6b
instrangeland <35543776+instrangeland@users.noreply.github.com>
2018-05-26 18:27:38 -0400

    Fixing Stuff and Debug

--- Receive/Receive.ino
+++ Receive/Receive.ino
@@ -1,16 +1,23 @@
 int ReadFrom=0;
 int x;
 int WriteTo=0;
+int a;
 int buffer[11];
 void setup() {
-  
+  Serial.begin(9600);
 
 }
 
 void loop() {
+  for(a=1;a!=10;1)
+  {
+    Serial.print(buffer[a]);
+    
+  }
+  Serial.println('a');
  if (buffer[ReadFrom]!=-1)
  {
-   Serial.write(buffer[ReadFrom]);
+   Serial.print(buffer[ReadFrom]);
    buffer[ReadFrom]=-1;
    ReadFrom++;
  }
